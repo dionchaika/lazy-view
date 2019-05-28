9a6d803ac3a2c01870cea03dfb4658cab3a7ca34
Dion Chaika <49035785+dionchaika@users.noreply.github.com>
2019-05-28 15:37:54 +0300

    Load source

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# lazy-view
-The Lazy Framework View Engine
+# Lazy View
+The Lazy Framework View Package
+
+## Requirements
+1. PHP 7.1.3
+
+## Installation
+Via composer:
+
+```bash
+composer require lazy/view
+```
---
+++ src/View.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Lazy\View;
+
+class View
+{
+    /**
+     * The view extentions.
+     */
+    const VIEW_EXT = [
+
+        '.view.js',
+        '.view.css',
+        '.view.php',
+        '.view.html'
+
+    ];
+
+    /**
+     * The compiled view extention.
+     */
+    const COMPILED_VIEW_EXT = '.compiled.php';
+
+    /**
+     * Enable compiled views cache.
+     *
+     * @var bool
+     */
+    public $enableCache = true;
+
+    /**
+     * The views root directory.
+     *
+     * @var string
+     */
+    protected $dir;
+
+    /**
+     * The compiled views root directory.
+     *
+     * @var string
+     */
+    protected $compiledDir;
+
+    /**
+     * @param string $dir
+     * @param string $compiledDir
+     */
+    public function __construct(string $dir, string $compiledDir = null)
+    {
+        $this->dir = rtrim($dir, \DIRECTORY_SEPARATOR);
+
+        if (null === $compiledDir) {
+            $this->compiledDir = $this->dir;
+        } else {
+            $this->compiledDir = rtrim($compiledDir, \DIRECTORY_SEPARATOR);
+        }
+    }
+
+    /**
+     * Get the views root directory.
+     *
+     * @return string
+     */
+    public function getDir(): string
+    {
+        return $this->dir;
+    }
+
+    /**
+     * Get the compiled views root directory.
+     *
+     * @return string
+     */
+    public function getCompiledDir(): string
+    {
+        return $this->compiledDir;
+    }
+
+    
+}
